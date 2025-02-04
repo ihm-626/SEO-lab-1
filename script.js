d1ddc22e362ae943ccb0be779d188f95c5410a4d
ihm-626 <iragajchenko@ukr.net>
2025-02-05 00:30:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const currentDate = new Date(); 
+console.log(currentDate); // реєструє поточну дату та час на консолі
+const targetDate = new Date('2025-04-29T10:43:45'); // Замінити цільовим 
+console.log(targetDate); // Перевірте цільову дату в консолі
+const difference = targetDate - currentDate; 
+console.log(difference); // Це зареєструє різницю в мілісекундах
+function updateCountdown() { 
+    const currentTime = new Date();
+    const difference = targetDate - currentTime; 
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24)); 
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); 
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)); 
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000); 
+    document.getElementById("days").innerText = days; 
+    document.getElementById("hours").innerText = hours; 
+    document.getElementById("minutes").innerText = minutes; 
+    document.getElementById("seconds").innerText = seconds;     
+    if (difference < 0) { 
+        clearInterval(interval); 
+        document.getElementById("timer").innerText = "The event has started!"; 
+        } 
+}
+const interval = setInterval(updateCountdown, 1000);
